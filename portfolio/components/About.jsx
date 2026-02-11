e694b767d17880f0edce1d4cafa69bb476b630e7
trinhhdinhh <trdinh321@gmail.com>
2026-02-11 11:28:22 -0600

    Checkpoint 1

---
+++ portfolio/components/About.jsx
@@ -0,0 +1,23 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <h4 className='text-center mb-2 text-lg font-ovo'>Introduction</h4>
+        <h2 className='text-center text-5xl font-ovo'>About Me</h2>
+
+        <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+            <div className='w-64 sm:w-80 lg:w-96 rounded-3xl max-w-none'>
+                <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
+            </div>
+            <div className='flex-1'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ portfolio/components/Navbar.jsx
@@ -0,0 +1,68 @@
+'use client'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React, { useRef } from 'react'
+import { Ovo } from 'next/font/google'
+
+const ovo = Ovo({
+  subsets: ['latin'],
+  weight: ['400']
+})
+
+const Navbar = () => {
+    
+    const sideMenuRef = useRef();
+
+    function openMenu() {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+    const closeMenu = ()=>{
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
+  return (
+    <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src= {assets.header_bg_color} alt='' className='w-full' />
+    </div>
+        <nav className={`${ovo.className} w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-between z-50 transition-colors duration-200`}>
+            <a href="#top" className='mr-14'>
+                <h1 className='text-4xl font-bold cursor-pointer text-black'>Trinh Dinh</h1>
+            </a>
+            <ul className='hidden md:flex item-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-gray-50 shadow-sm transition-colors duration-200'>
+                <li><a className='font-Ovo' href="#top">Home</a></li>
+                <li><a className='font-Ovo' href="#about">About</a></li>
+                <li><a className='font-Ovo' href="#services">Services</a></li>
+                <li><a className='font-Ovo' href="#work">My Work</a></li>
+                <li><a className='font-Ovo' href="#contact">Contact Me</a></li>
+            </ul>
+            <div className='flex items-center gap-4'>
+                <button className='cursor-pointer'>
+                    <Image src={assets.moon_icon} alt='' className='w-6'/>
+                </button>
+
+                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full hover:bg-gray-50 text-gray-700 transition-colors duration-200'>Contact <Image src={assets.arrow_icon} alt="" className='w-3'/>
+                </a>
+                <button className='block md:hidden ml-3 cursor-pointer' onClick={openMenu}>
+                    <Image src={assets.menu_black} alt='' className='w-6'/>
+                </button>
+            </div>
+
+            {/* ---------- Mobile Menu ---------- */}
+            <ul ref ={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
+                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                </div>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#about">About</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#services">Services</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#work">My Work</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#contact">Contact Me</a></li>
+            </ul>
+
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
